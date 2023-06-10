6bcecc6ed5dcda2877d924c52645be8597a9c54e
n-o-o-b-z <dexmacone@gmail.com>
2023-06-10 18:51:40 +0800

    this is the old version

--- insertionSort.php
+++ insertionSort.php
@@ -1,23 +1,4 @@
 <?php
 
-$arr = array(5, 2, 8, 3, 1);
-
-$sortedArr = insertionSort($arr);
-
-echo implode(", ", $sortedArr); 
-
-function insertionSort($arr) {
-    $n = count($arr);
-    for ($i = 1; $i < $n; $i++) {
-      $key = $arr[$i];
-      $j = $i - 1;
-      while ($j >= 0 && $arr[$j] > $key) {
-        $arr[$j + 1] = $arr[$j];
-        $j--;
-      }
-      $arr[$j + 1] = $key;
-    }
-    return $arr;
-  }
-
+echo "this is the old";
 ?>
