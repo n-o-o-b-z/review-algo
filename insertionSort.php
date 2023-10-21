bd5d58241bf574f3bf733e8988c1febaff4aa45d
n-o-o-b-z <dexmacone@gmail.com>
2023-10-21 20:23:21 +0800

    asdasdasda

--- insertionSort.php
+++ insertionSort.php
@@ -1,4 +1,23 @@
 <?php
 
-echo "new branch here";
+$arra = array(5, 2, 8, 3, 1);
+
+
+insertionSort($arra);
+
+
+function insertionSort($arr) {
+    $n = count($arr);
+    for ($i = 1; $i < $n; $i++) {
+        $key = $arr[$i];
+        $j = $i - 1;
+        while ($j >= 0 && $arr[$j] > $key) {
+            $arr[$j + 1] = $arr[$j];
+            $j--;
+        }
+        $arr[$j + 1] = $key;
+    }
+    return $arr;
+  }
+
 ?>
---
+++ sumTriple.php
@@ -0,0 +1,15 @@
+<?php
+
+$number = 51;
+$givenNumber = 51;
+
+
+$total = $number - $givenNumber;
+
+if($number >= $givenNumber){
+    echo $total * 3;
+}else {
+    echo $total;
+}
+
+?>
